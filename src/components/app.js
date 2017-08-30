a7e91cb4810859c73770455e3ac9752e515f4346
Soumik Sur <soumiksur.me@gmail.com>
2017-08-30 10:49:34 +0530

    done till ajax request

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import SearchBar from '../container/search_bar'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<SearchBar />
+      </div>
+    );
+  }
+}
