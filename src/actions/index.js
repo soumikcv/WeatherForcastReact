1a1cee67733594b8b973758e5f14adaa605829aa
Soumik Sur <soumiksur.me@gmail.com>
2017-08-30 12:08:50 +0530

    made charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,12 +6,12 @@ const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${AP
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
-	const url = `${ROOT_URL}q=${city},us`;
+	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
 
 	return{
 		type: FETCH_WEATHER,
 		payload: request
 	};
-	
+
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../container/search_bar'
+import WeatherList from '../container/weather_list'
 export default class App extends Component {
   render() {
     return (
       <div>
       	<SearchBar />
-      </div>
+      	<WeatherList />
+       </div>
     );
   }
 }
